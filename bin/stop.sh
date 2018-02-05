ac7ab9d7b19be9634a2d803f74ea640b809f6f83
RPCheung <iwanttobeaprince@gmail.com>
2018-02-05 12:42:14 +0800

    新增 rmi 控制

--- bin/stop.sh
+++ bin/stop.sh
@@ -1,2 +1,20 @@
-kill -9 2657
+# 回退两层
+#KEY_PATH=$(dirname "$(dirname "$PWD")")
+# 回退一层~
+SIP_HOME=$(dirname "$PWD")
+export SIP_HOME
+
+JAVA_OPTS='-server -Xmx1024m -Xms1024m -Xmn64m -Xss32m'
+export JAVA_OPTS
+
+# for i in ${SERVER_HOME}/conf/* 
+# do CLASSPATH=$i:"$CLASSPATH"; 
+# done 
+#echo $CLASSPATH
+
+# export CLASSPATH=$CLASSPATH:${SERVER_HOME}/conf/
+#-Djava.ext.dirs	~
+#
+
+java ${JAVA_OPTS}  -classpath ${SIP_HOME}/work/sip/lib/script/sip-controller.jar com.rp.sip.SipController
  echo sip has stopped.
